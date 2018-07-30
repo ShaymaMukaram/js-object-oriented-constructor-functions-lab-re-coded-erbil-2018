3003c0ae4d56c5ae5a73bfc9755d0422d32221ef
Shayma Mukaram <shaymamukaram@gmail.com>
2018-07-30 08:35:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,9 @@ function Driver(name,age,experience){
 }
 
 let allison = new Driver("Allison", "16 years", "2 years");
-
-
+function PickupLocation(address,city){
+  this.address=address;
+  this.city=city;
+  
+}
+let poshHotel = new PickupLocation("123 Broadway", "New York City");
